461fd15766a13ce38ebfa0d08bbfbfdf5e8e3313
Gus <gus@guscuddy.com>
2025-11-30 16:08:15 -0500

    shadcn rac

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,32 @@
-import process from 'node:process'
+import process from "node:process";
+import path from "path";
 
-import { cloudflare } from '@cloudflare/vite-plugin'
-import { livestoreDevtoolsPlugin } from '@livestore/devtools-vite'
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { livestoreDevtoolsPlugin } from "@livestore/devtools-vite";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 60_001,
     fs: { strict: false },
   },
-  worker: { format: 'es' },
+  worker: { format: "es" },
   plugins: [
     cloudflare(),
     tailwindcss(),
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
     }),
     react(),
-    livestoreDevtoolsPlugin({ schemaPath: './src/livestore/schema.ts' }),
+    livestoreDevtoolsPlugin({ schemaPath: "./src/livestore/schema.ts" }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
